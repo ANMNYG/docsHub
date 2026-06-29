e49d1a6a734328657be941372360928ffdf32ee3
ANMNYG <dkslasdud@gmail.com>
2026-06-29 10:34:13 +0900

    Initial Commit

---
+++ src/test/java/com/devtool/docshub/DocsHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtool.docshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
