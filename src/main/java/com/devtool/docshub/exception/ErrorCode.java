dc7cac5af7d339ddd3d9d1ce2878be5197f62181
ANMNYG <dkslasdud@gmail.com>
2026-07-01 23:00:01 +0900

    [feat] ErrorCode와 BusinessException 추가 ErrorCode Enum을 통해 관리할 에러 유형 정의 비지닛 로직 에러 처리를 위한 BusinessException 클래스 추가

---
+++ src/main/java/com/devtool/docshub/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.devtool.docshub.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    // 여기에 에러 종류를 추가하면 됩니다.
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 입력값입니다."),
+    DOCUMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 문서를 찾을 수 없습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
